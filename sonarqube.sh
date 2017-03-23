64d23195919d5688b4fa3b2c1ff9c0943d0a2473
Maximilian <maximilian.voelker@student.hpi.de>
2017-03-23 11:02:04 +0100

    add inclusion command for sonarqube

--- sonarqube.sh
+++ sonarqube.sh
@@ -5,6 +5,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         -Dsonar.host.url=$SONAR_HOST_URL \
         -Dsonar.login=$SONAR_AUTH_TOKEN \
         -Dsonar.branch=$TRAVIS_BRANCH \
+        -Dsonar.test.inclusions=src/test/java/de/hpi/unicorn/application/pages/input/generator/**/* \
 		    -Dsonar.inclusions=src/main/java/de/hpi/unicorn/application/pages/input/generator/**/* \
 
 else
@@ -15,7 +16,8 @@ else
         -Dsonar.analysis.mode=preview \
         -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
         -Dsonar.github.oauth=$GITHUB_ACCESS_TOKEN \
-        -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST
+        -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+        -Dsonar.test.inclusions=src/test/java/de/hpi/unicorn/application/pages/input/generator/**/* \
         -Dsonar.inclusions=src/main/java/de/hpi/unicorn/application/pages/input/generator/**/* \
 
 fi
