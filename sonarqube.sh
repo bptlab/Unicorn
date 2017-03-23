12b6ad4326abd0ed25a3f5c414f519de14d46421
MaximilianV <MaximilianV@users.noreply.github.com>
2017-03-23 12:57:26 +0100

    Sonarqube support (#7)
    * first travis / sonarqube try
    
    * add before install step
    
    * rename travis yml with dot
    
    * make sonarqube script run
    
    * first try to only check generator package in sonarqube
    
    * Limit travis on own packages
    
    * try sonarqube support
    
    * try relative path for sonarqube
    
    * fix wildcard in relative path
    
    * add inclusion command for sonarqube
    
    * add more test paths to include json import and dependencies
    
    * next try with multiple inclusions

---
+++ sonarqube.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    mvn verify sonar:sonar -DskipTests \
+        -Dsonar.host.url=$SONAR_HOST_URL \
+        -Dsonar.login=$SONAR_AUTH_TOKEN \
+        -Dsonar.branch=$TRAVIS_BRANCH \
+        -Dsonar.test.inclusions=src/test/java/de/hpi/unicorn/application/pages/input/generator/**,src/test/java/de/hpi/unicorn/json/importer/** \
+		    -Dsonar.inclusions=src/main/java/de/hpi/unicorn/application/pages/input/generator/**,src/main/java/de/hpi/unicorn/attributeDependency/**,src/main/java/de/hpi/unicorn/importer/json/** \
+
+else
+    mvn verify sonar:sonar -DskipTests \
+        -Dsonar.host.url=$SONAR_HOST_URL \
+        -Dsonar.login=$SONAR_AUTH_TOKEN \
+        -Dsonar.branch=$TRAVIS_BRANCH \
+        -Dsonar.analysis.mode=preview \
+        -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+        -Dsonar.github.oauth=$GITHUB_ACCESS_TOKEN \
+        -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+        -Dsonar.test.inclusions=src/test/java/de/hpi/unicorn/application/pages/input/generator/**,src/test/java/de/hpi/unicorn/json/importer/** \
+		    -Dsonar.inclusions=src/main/java/de/hpi/unicorn/application/pages/input/generator/**,src/main/java/de/hpi/unicorn/attributeDependency/**,src/main/java/de/hpi/unicorn/importer/json/** \
+
+fi
