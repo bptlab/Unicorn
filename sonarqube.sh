be6974f42d33b7ef44dc00628024d6f4221fe7b1
Maximilian <maximilian.voelker@student.hpi.de>
2017-03-23 11:18:46 +0100

    add more test paths to include json import and dependencies

--- sonarqube.sh
+++ sonarqube.sh
@@ -5,8 +5,13 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         -Dsonar.host.url=$SONAR_HOST_URL \
         -Dsonar.login=$SONAR_AUTH_TOKEN \
         -Dsonar.branch=$TRAVIS_BRANCH \
-        -Dsonar.test.inclusions=src/test/java/de/hpi/unicorn/application/pages/input/generator/**/* \
-		    -Dsonar.inclusions=src/main/java/de/hpi/unicorn/application/pages/input/generator/**/* \
+        -Dsonar.test.inclusions= \
+          src/test/java/de/hpi/unicorn/application/pages/input/generator/**/*, \
+          src/test/java/de/hpi/unicorn/json/importer/**/* \
+		    -Dsonar.inclusions= \
+          src/main/java/de/hpi/unicorn/application/pages/input/generator/**/*, \
+          src/main/java/de/hpi/unicorn/attributeDependency/**/*, \
+          src/main/java/de/hpi/unicorn/importer/json/**/*
 
 else
     mvn verify sonar:sonar -DskipTests \
@@ -17,7 +22,12 @@ else
         -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
         -Dsonar.github.oauth=$GITHUB_ACCESS_TOKEN \
         -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
-        -Dsonar.test.inclusions=src/test/java/de/hpi/unicorn/application/pages/input/generator/**/* \
-        -Dsonar.inclusions=src/main/java/de/hpi/unicorn/application/pages/input/generator/**/* \
+        -Dsonar.test.inclusions= \
+          src/test/java/de/hpi/unicorn/application/pages/input/generator/**/*, \
+          src/test/java/de/hpi/unicorn/json/importer/**/* \
+		    -Dsonar.inclusions= \
+          src/main/java/de/hpi/unicorn/application/pages/input/generator/**/*, \
+          src/main/java/de/hpi/unicorn/attributeDependency/**/*, \
+          src/main/java/de/hpi/unicorn/importer/json/**/*
 
 fi
