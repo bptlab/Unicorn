e2f0a4c5b2b193c6941a7a8b0c43b5390c40beb8
Maximilian <maximilian.voelker@student.hpi.de>
2017-03-23 10:46:04 +0100

    fix wildcard in relative path

--- sonarqube.sh
+++ sonarqube.sh
@@ -5,7 +5,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         -Dsonar.host.url=$SONAR_HOST_URL \
         -Dsonar.login=$SONAR_AUTH_TOKEN \
         -Dsonar.branch=$TRAVIS_BRANCH \
-		    -Dsonar.inclusions=src/main/java/de/hpi/unicorn/application/pages/input/generator* \
+		    -Dsonar.inclusions=src/main/java/de/hpi/unicorn/application/pages/input/generator/**/* \
 
 else
     mvn verify sonar:sonar -DskipTests \
@@ -16,6 +16,6 @@ else
         -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
         -Dsonar.github.oauth=$GITHUB_ACCESS_TOKEN \
         -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST
-        -Dsonar.inclusions=src/main/java/de/hpi/unicorn/application/pages/input/generator* \
+        -Dsonar.inclusions=src/main/java/de/hpi/unicorn/application/pages/input/generator/**/* \
 
 fi
