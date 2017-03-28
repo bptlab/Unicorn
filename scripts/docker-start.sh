b7a524865133787a2ede93029ea80edeaa44a8da
Tom Martensen <tom.martensen@student.hpi.uni-potsdam.de>
2017-03-28 13:00:51 +0200

    makes the user and password configurable for docker

--- scripts/docker-start.sh
+++ scripts/docker-start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-export CATALINA_OPTS="-Ddb.host=${MYSQL_PORT_3306_TCP_ADDR} -Ddb.port=${MYSQL_PORT_3306_TCP_PORT}"
+export CATALINA_OPTS="-Ddb.host=${MYSQL_PORT_3306_TCP_ADDR} -Ddb.port=${MYSQL_PORT_3306_TCP_PORT} -Ddb.user=${MYSQL_ENV_MYSQL_USER} -Ddb.password=${MYSQL_ENV_MYSQL_PASSWORD}"
+export
 catalina.sh run
