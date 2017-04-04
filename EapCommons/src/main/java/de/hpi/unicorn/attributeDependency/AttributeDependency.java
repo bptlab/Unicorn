67218b8a5d8f60e029f5f3bb6ea7a9affb05d3aa
Maximilian <maximilian.voelker@student.hpi.de>
2017-04-04 16:00:39 +0200

    SonarQube imports in dependencies

--- EapCommons/src/main/java/de/hpi/unicorn/attributeDependency/AttributeDependency.java
+++ EapCommons/src/main/java/de/hpi/unicorn/attributeDependency/AttributeDependency.java
@@ -24,6 +24,17 @@ import javax.persistence.Query;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Query;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
 import java.util.Map;
 import java.util.List;
 
