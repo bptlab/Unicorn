1bec2b5d9fdaba2907919a0580a5b8ea496bd087
Maximilian <maximilian.voelker@student.hpi.de>
2017-04-18 10:16:49 +0200

    add javadocs for attributeValueDependency

--- EapCommons/src/main/java/de/hpi/unicorn/attributeDependency/AttributeValueDependency.java
+++ EapCommons/src/main/java/de/hpi/unicorn/attributeDependency/AttributeValueDependency.java
@@ -51,10 +51,20 @@ public class AttributeValueDependency extends Persistable {
     @Column(name = "DependentAttributeValues")
     private String dependentAttributeValues;
 
+    /**
+     * Basic constructor for AttributeValueDependency (no ID given, defaults to 0).
+     */
     public AttributeValueDependency() {
         this.ID = 0;
     }
 
+    /**
+     * Default constructor for AttributeValueDependency.
+     *
+     * @param dependencyRule a AttributeDependency this value dependency should be defined in
+     * @param baseAttributeValue contains the value triggering this dependency
+     * @param dependentAttributeValues contains the values that should be used within this rule for the dependent attribute value
+     */
     public AttributeValueDependency(AttributeDependency dependencyRule, String baseAttributeValue, String dependentAttributeValues) {
         this();
         this.dependencyRule = dependencyRule;
@@ -62,21 +72,52 @@ public class AttributeValueDependency extends Persistable {
         this.dependentAttributeValues = dependentAttributeValues;
     }
 
+    /**
+     * Getter for ID of dependency.
+     *
+     * @return an integer
+     */
     @Override
     public int getID() {
         return this.ID;
     }
 
+    /**
+     * Getter for the value triggering this dependency rule.
+     *
+     * @return a string containing the value of the base attribute
+     */
     public String getBaseAttributeValue() { return this.baseAttributeValue; }
 
+    /**
+     * Getter for the values that can be used within this rule for the dependent attribute.
+     *
+     * @return a string containing the dependent attribute value in this rule
+     */
     public String getDependentAttributeValues() { return this.dependentAttributeValues; }
 
+    /**
+     * Setter for the values that can be used within this rule for the dependent attribute.
+     *
+     * @param dependentAttributeValues new values for the dependent attribute
+     */
     public void setDependentAttributeValues(String dependentAttributeValues) {
         this.dependentAttributeValues = dependentAttributeValues;
     }
 
+    /**
+     * Getter for the AttributeDependency-rule this value dependency is defined on.
+     *
+     * @return an AttributeDependency
+     */
     public AttributeDependency getDependencyRule() { return this.dependencyRule; }
 
+    /**
+     * Static method to return all AttributeValueDependencies defined on a given AttributeDependency.
+     *
+     * @param attributeDependency the AttributeValueDependencies should be returned for
+     * @return a list with AttributeValueDependencies
+     */
     public static List<AttributeValueDependency> getAttributeValueDependenciesFor(AttributeDependency attributeDependency) {
         final Query query = Persistor.getEntityManager().createQuery("SELECT a FROM AttributeValueDependency a WHERE a.dependencyRule = "
                 + ":depRule", AttributeValueDependency.class).setParameter("depRule", attributeDependency);
@@ -86,6 +127,7 @@ public class AttributeValueDependency extends Persistable {
     /**
      * Find the value dependency with the given dependency rule and base value.
      * As this combination is a primary key, there is just one value dependency returned.
+     * 
      * @param attributeDependency AttributeDependency of searched value dependency
      * @param baseValue base value of searched value dependency
      * @return value dependency that matches the search criteria
