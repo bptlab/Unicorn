bd6217f46c67f3c13f39a1ccf7bca7dcede49be8
Christian Friedow <christian@friedow.com>
2017-05-18 15:02:44 +0200

    Update docker-start.sh

--- scripts/docker-start.sh
+++ scripts/docker-start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export CATALINA_OPTS="-Ddb.host=${MYSQL_PORT_3306_TCP_ADDR} -Ddb.port=${MYSQL_PORT_3306_TCP_PORT} -Ddb.user=${MYSQL_ENV_MYSQL_USER} -Ddb.password=${MYSQL_ENV_MYSQL_PASSWORD}"
+export CATALINA_OPTS="-Ddb.host=database -Ddb.port=3306 -Ddb.user=${MYSQL_USER} -Ddb.password=${MYSQL_ROOT_PASSWORD}"
 catalina.sh run
