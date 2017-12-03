5cec23f12b20426fae8315595a7287d9217104fa
m0r13 <moritz.hilscher@gmail.com>
2017-12-03 23:47:52 +0200

    Add UNICORN_DEPLOY_NAME arg to docker container for web context

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM tomcat:8.5.11-jre8
 RUN apt-get update && apt-get install -y gettext-base && \
     cp /usr/local/tomcat/conf/server.xml /usr/local/tomcat/conf/server-template.xml
 
-COPY EapWebInterface/target/Unicorn.war /usr/local/tomcat/webapps/Unicorn.war
+COPY EapWebInterface/target/Unicorn.war /Unicorn.war
 COPY unicorn.properties.tpl /usr/local/tomcat/conf/unicorn.properties.tpl
 COPY scripts/docker-start.sh /usr/local/bin/docker-start.sh
 RUN chmod +x /usr/local/bin/docker-start.sh
--- scripts/docker-start.sh
+++ scripts/docker-start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -z ${UNICORN_DEPLOY_NAME+x} ]; then
+    export UNICORN_DEPLOY_NAME=Unicorn
+fi
+
 if [ -z ${UNICORN_DB_PORT+x} ]; then
     export UNICORN_DB_PORT=3306
 fi
@@ -12,6 +16,7 @@ if [ -z ${UNICORN_DB_TEST_DB+x} ]; then
     export UNICORN_DB_TEST_DB=eap_testing
 fi
 
+cp /Unicorn.war /usr/local/tomcat/webapps/$UNICORN_DEPLOY_NAME.war
 envsubst < /usr/local/tomcat/conf/unicorn.properties.tpl > /usr/local/tomcat/conf/unicorn.properties
 # TODO why this?!
 #envsubst < /usr/local/tomcat/conf/server-template.xml > /usr/local/tomcat/conf/server.xml
