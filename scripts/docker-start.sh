2955145e2d7fcb0470a3a2ca2074938e7f375d9c
m0r13 <moritz.hilscher@gmail.com>
2018-10-05 16:31:55 +0200

    s/password/passw/ in docker-start.sh

--- scripts/docker-start.sh
+++ scripts/docker-start.sh
@@ -20,6 +20,6 @@ cp /Unicorn.war /usr/local/tomcat/webapps/$UNICORN_DEPLOY_NAME.war
 # let's do this to make unicorn happy
 envsubst < /usr/local/tomcat/conf/unicorn.properties.tpl > /usr/local/tomcat/conf/unicorn.properties
 # but db connection data is actually taken from here
-export CATALINA_OPTS="-Ddb.host=$UNICORN_DB_HOST -Ddb.port=$UNICORN_DB_PORT -Ddb.dev.name=$UNICORN_DB_DEV_DB -Ddb.user=$UNICORN_DB_USER -Ddb.password=$UNICORN_DB_PASSWORD"
+export CATALINA_OPTS="-Ddb.host=$UNICORN_DB_HOST -Ddb.port=$UNICORN_DB_PORT -Ddb.dev.name=$UNICORN_DB_DEV_DB -Ddb.user=$UNICORN_DB_USER -Ddb.passw=$UNICORN_DB_PASSWORD"
 catalina.sh run
 
