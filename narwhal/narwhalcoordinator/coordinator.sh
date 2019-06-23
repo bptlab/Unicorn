82eda489d5175707ddffa8ab082177601f52dcf0
florian <florian.papsdorf@student.hpi.de>
2019-06-23 23:34:29 +0200

    Fix some bugs between coord & tomcat

--- narwhal/narwhalcoordinator/coordinator.sh
+++ narwhal/narwhalcoordinator/coordinator.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
-# TODO: look at necessary commandline arguments
-python3 narwhalvalues.py
-python3 unicornproperties.py
+# starts in /home
+
+python3 narwhalvalues.py -s 1 -f configs/
+# python3 unicornproperties.py
+
+# wait until raise flag that all config files are prepared
+while [ ! -f "./configs/coordinator.work" ]
+do
+    sleep 1s
+done
+# delete flag raised by tomcat
+rm ./configs/coordinator.work
+python3 tomcatproperties.py -p configs/
+# signalize tomcat that config files are ready
+touch ./configs/coordinator.finished
--- narwhal/tomcat-srv/tomcatscript.sh
+++ narwhal/tomcat-srv/tomcatscript.sh
@@ -2,25 +2,25 @@
 
 cd /home/configs
 
-# get original web.xml as template for narwhalcoordinator
+# get original config files as template for narwhalcoordinator
 cp /usr/local/tomcat/webapps/manager/WEB-INF/web.xml ./web-template.xml
+cp /usr/local/tomcat/conf/tomcat-users.xml ./tomcat-users-template.xml
 
-# wait until narwhalcoordinator generates them
-while [ ! -f "./web.xml" ]
+# signalize coordinator to do his job
+touch coordinator.work
+
+# wait until narwhalcoordinator signalize it's finished
+while [ ! -f "./coordinator.finished" ]
 do
     sleep 1s
 done
 
-while [ ! -f "./tomcat-users.xml" ]
-do
-    sleep 1s
-done
-
-sleep 0.5s
+# delete flag raised by coordinator
+rm coordinator.finished 
 
 # push config files to their place
 cp ./web.xml /usr/local/tomcat/webapps/manager/WEB-INF/web.xml
 cp ./tomcat-users.xml /usr/local/tomcat/conf/tomcat-users.xml
 
 # start Tomcat server like original container CMD
-./usr/local/tomcat/bin/catalina.sh
+/usr/local/tomcat/bin/catalina.sh run
