e19e48db4100341b455ffc0ce0db69049ea0aa15
florian <florian.papsdorf@student.hpi.de>
2019-06-25 22:43:36 +0200

    Handle unicorn.properties inside tomcat container

--- narwhal/tomcat-srv/tomcatscript.sh
+++ narwhal/tomcat-srv/tomcatscript.sh
@@ -21,6 +21,7 @@ rm coordinator.finished
 # push config files to their place
 cp ./web.xml /usr/local/tomcat/webapps/manager/WEB-INF/web.xml
 cp ./tomcat-users.xml /usr/local/tomcat/conf/tomcat-users.xml
+cp ./unicorn.properties /home/unicorn.properties
 
 # start Tomcat server like original container CMD
 /usr/local/tomcat/bin/catalina.sh run
