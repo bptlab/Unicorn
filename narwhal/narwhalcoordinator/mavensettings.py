52c209f69a12f7edfe71738b044adc6cd9d5bc11
florian <florian.papsdorf@student.hpi.de>
2019-06-26 10:39:23 +0200

    Implement module to create maven settings

--- narwhal/narwhalcoordinator/mavensettings.py
+++ narwhal/narwhalcoordinator/mavensettings.py
@@ -21,19 +21,40 @@ class NarwhalMavenSettings:
         }
         return keyValuePairs
 
-    def __init__(self):
+    def __init__(self, narwhalvalues, destination):
+        self.narwhalValPath = narwhalvalues
+        self.destinationpath = destination
         self.settingsStructure = None
+        self.generateStructure(NarwhalMavenSettings.collectAttributes(self.narwhalValPath))
 
     def generateStructure(self, serverconfig):
         self.settingsStructure = xmlWorker.Element("settings")
         for subtag in NarwhalMavenSettings.settingsTags:
             tmp = xmlWorker.SubElement(self.settingsStructure, subtag)
             if subtag == "servers":
-                pass
+                for serverKey in serverconfig:
+                    xmlWorker.SubElement(tmp, serverKey).text = serverconfig[serverKey]
+            else:
+                tmp.append(xmlWorker.Comment(
+                    "Statements for {} configs".format(subtag)
+                ))
 
+    def writeStructureToFile(self):
+        output = xmlWorker.tostring(self.settingsStructure, encoding='utf8').decode('utf8')
+        fileHanlder = open(self.destinationpath + "settings.xml", "w")
+        fileHanlder.write(output)
+        fileHanlder.close()
 
 def main():
-    pass
+    cmdArgsHandler = argparse.ArgumentParser(description="Module to adapt mavens settings.xml")
+    cmdArgsHandler.add_argument("-n", "--narwhalvaluespath", type=str, default="", help="Path to find the narwhalproperties.json")
+    cmdArgsHandler.add_argument("-d", "--destinationpath", type=str, default="", help="Path to store the final settings.xml")
+    parsedArgs = cmdArgsHandler.parse_args()
+    mavenSettingsManager = NarwhalMavenSettings(
+        parsedArgs.narwhalvaluespath,
+        parsedArgs.destinationpath
+    )
+    mavenSettingsManager.writeStructureToFile()
 
 if __name__ == "__main__":
     main()
